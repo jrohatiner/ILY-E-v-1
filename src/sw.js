cee54951c3e5772a1619e24cd871ad4e7bf80126
jrohatiner <judithrohatiner@gmail.com>
2023-08-16 11:30:52 -0400

    1 binaries uploaded

---
+++ src/sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'ily-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/js/main.js',
+  '/styles.scss',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
